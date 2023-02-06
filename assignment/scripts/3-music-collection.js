43a19ef6dadc9743876305ac9c0cc2a3a2a2d8db
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-02-06 14:28:16 -0600

    Added 6 albums, logged now newly populated collections array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -20,3 +20,11 @@ function addToCollection (title, artist, yearPublished) {
     return newRecord;
 }
 
+console.log(addToCollection('Ballads', 'John Coltrane', 1963));
+console.log(addToCollection('My Favorite Things', 'John Coltrane', 1961));
+console.log(addToCollection("Cher's Golden Greats", 'Cher', 1968));
+console.log(addToCollection('Interventions', 'Horse Lords', 2016));
+console.log(addToCollection('The Bad Plus Joshua Redman', 'The Bad Plus and Joshua Redman', 2016));
+console.log(addToCollection('El Mal Querer', 'Rosalia', 2018));
+
+console.log(collection);

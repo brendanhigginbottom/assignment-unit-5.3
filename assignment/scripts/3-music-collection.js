ad2a7d08b4e5bb3e640a5619bc4c5187d9518530
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-02-06 14:11:36 -0600

    Created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,22 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+/**
+ * 
+ * @param {string} title Title of album to add to a new object
+ * @param {string} artist Artist name to add to a new object
+ * @param {number} yearPublished Publication year to add to a new object
+ * @returns {object} Returns object with the above key-value pairs
+ */
+
+function addToCollection (title, artist, yearPublished) {
+    let newRecord = {
+        albumTitle: title,
+        albumArtist: artist,
+        albumYearPublished: yearPublished
+    }
+    collection.push(newRecord);
+    return newRecord;
+}
+

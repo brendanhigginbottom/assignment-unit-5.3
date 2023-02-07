ed612e510fa5a48b2e41c2d9029f406727b7a3a2
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-02-07 10:52:32 -0600

    Added two tracks to each album. Rewrote showCollection function as 3-part for loop and updated to list track names. Need to update other two functions

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,27 +7,31 @@ let collection = [];
  * @param {string} title Title of album to add to a new object
  * @param {string} artist Artist name to add to a new object
  * @param {number} yearPublished Publication year to add to a new object
+ * @param {array} tracks Array of objects, each object in the array containing a track name and duration
  * @returns {object} Returns object with the above key-value pairs
  */
 
-function addToCollection (title, artist, yearPublished) {
+function addToCollection (title, artist, yearPublished, tracks) {
     let newRecord = {
         albumTitle: title,
         albumArtist: artist,
-        albumYearPublished: yearPublished
+        albumYearPublished: yearPublished,
+        albumTracks: tracks
     }
     collection.push(newRecord);
     return newRecord;
 }
 
-console.log(addToCollection('Ballads', 'John Coltrane', 1963));
-console.log(addToCollection('My Favorite Things', 'John Coltrane', 1961));
-console.log(addToCollection("Cher's Golden Greats", 'Cher', 1968));
-console.log(addToCollection('Interventions', 'Horse Lords', 2016));
-console.log(addToCollection('The Bad Plus Joshua Redman', 'The Bad Plus and Joshua Redman', 2016));
-console.log(addToCollection('El Mal Querer', 'Rosalia', 2018));
+console.log(addToCollection('Ballads', 'John Coltrane', 1963, [{name: "What's New", duration: '3:44'}, {name: 'All Or Nothing At All', duration: '3:34'}]));
+console.log(addToCollection('My Favorite Things', 'John Coltrane', 1961, [{name: 'But Not for Me', duration: '9:35'}, {name: 'My Favorite Things', duration: '13:45'}]));
+console.log(addToCollection("Cher's Golden Greats", 'Cher', 1968, [{name: 'All I really Want to Do', duration: '2:58'}, {name: 'Sunny', duration: '3:09'}]));
+console.log(addToCollection('Interventions', 'Horse Lords', 2016, [{name: 'Encounters I / Transfinite Flow', duration: '3:26'}, {name: 'Time Slip', duration: '2:33'}]));
+console.log(addToCollection('The Bad Plus Joshua Redman', 'The Bad Plus and Joshua Redman', 2016, [{name: 'As This Moment Slips Away', duration: '6:55'}, {name: 'Friend or Foe', duration: '8:39'}]));
+console.log(addToCollection('El Mal Querer', 'Rosalia', 2018, [{name: 'MALDICION', duration: '2:55'}, {name: 'MALAMENTE', duration: '2:30'}]));
 
 console.log(collection);
+
+console.log(collection[0].albumTracks.length);
 
 /**
  * 
@@ -36,19 +40,25 @@ console.log(collection);
 
 function showCollection (collectionArray) {
     console.log(collectionArray.length);
-    for (let albumInfo of collectionArray) {
-        console.log(`${albumInfo.albumTitle} by ${albumInfo.albumArtist} published in ${albumInfo.albumYearPublished}`)
-    };
+    let initialTrackNumber = 1;
+    for (let i = 0; i < collectionArray.length; i += 1) {
+        console.log(`${collectionArray[i].albumTitle} by ${collectionArray[i].albumArtist}, published in ${collectionArray[i].albumYearPublished}:`);
+        for (let j = 0; j < collectionArray[i].albumTracks.length; j += 1) {
+            console.log(`${initialTrackNumber}. ${collectionArray[i].albumTracks[j].name}: ${collectionArray[i].albumTracks[j].duration}`);
+            initialTrackNumber += 1;
+        }
+    initialTrackNumber = 1;
+    }
 }
-
 showCollection(collection);
+
 
 /**
  * 
  * @param {string} artist Artist to search collection array for
  * @returns {array} Array of matches by artist name, empty array if no matches found
  */
-
+/* here
 function findByArtist(artist) {
     let matchingArray = [];
     for (let match of collection) {
@@ -67,7 +77,7 @@ console.log(findByArtist('Bad Bunny')); // Should return no matches/an empty arr
  * @param {object} object Object to search collection array for full matches. keys that can be (not must) passed in object are artist, album, year.
  * @returns {array} Array of matching album info, empty array if no matches, full collection array if no object key-value pairs provided or no object pass as an argument at all.
  */
-
+/* here
 function search(object) {
     // first need to test if object is undefined, otherwise I get an error when declaring searchItems equal to no object. Works with an empty object though. Returning before all the other code avoids the error.
     if (object === undefined) {
@@ -114,3 +124,5 @@ console.log(search({artist: 'John Coltrane'}));
 console.log(search({}));
 console.log(search({artist: 'Bad Bunny'}));
 console.log(search());
+
+*/ // here
